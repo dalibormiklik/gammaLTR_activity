6bf622ac9443794cfa8b79ade3b8a016b0a6e639
dalibormiklik <112612710+dalibormiklik@users.noreply.github.com>
2024-06-14 15:22:38 +0200

    Update Figure3_B_FACS_3dpi.R

--- code/Figures/Figure3_B_FACS_3dpi.R
+++ code/Figures/Figure3_B_FACS_3dpi.R
@@ -25,8 +25,10 @@ var.lvls <- c("wt", "W390A", "CBX")
 #load tables
 
 #Extract data for each measured cell from FACS
+#Due to the large size of the original file exported from the FlowJo, modified 'Cell_Export_2.txt' - originally the 'data.to.plot' data.frame file is loaded instead
+#File contains max. 10k cell per sample randomly selected by the 'sample()' function (see additional code below)
 export.dir <- data.path
-export.file <- "Cell_Export.txt"
+export.file <- "Cell_Export_2.txt"
 
 #Run 'join_FCS_data.R' on raw FlowJo-exported files to create export.file
 #if(!file.exists(paste0(export.dir, export.file))) {
@@ -34,44 +36,53 @@ export.file <- "Cell_Export.txt"
 #}
 
 export.df <- read.table(paste0(export.dir, export.file),
-                        sep = "\t", header = TRUE, quote = "", stringsAsFactors = FALSE,
-                        col.names = c("FSC.A", "FSC.H", "SSC.A", "SSC.H", "GFP", "Hoechst", "mCherry", "Time", "Virus", "Variant", "dpi", "Population"))
+                        sep = "\t", header = TRUE, quote = "", stringsAsFactors = FALSE)
 
 #set data.to.plot
 sel.var <- c("wt", "W390A", "CBX")
 sel.dpi <- 3
-export.df$GID <- paste0(export.df$Virus, "_", export.df$Variant, "_", export.df$dpi)
 
-sel.export.df <- export.df[!is.na(export.df$Variant) &
-                             !is.na(export.df$Variant) &
-                             export.df$Variant %in% sel.var &
-                             export.df$Virus %in% vir &
-                             export.df$dpi == sel.dpi,]
-sel.export.df$Virus <- factor(sapply(sel.export.df$Virus,
-                                    function(v) {
-                                      vir.lvls[which(vir == v)]
-                                    }),
-                             levels = vir.lvls)
+#This code is included when the original 'Cell_Export.txt' table is loaded
+#export.df$GID <- paste0(export.df$Virus, "_", export.df$Variant, "_", export.df$dpi)
+#
+#sel.export.df <- export.df[!is.na(export.df$Variant) &
+#                             !is.na(export.df$Variant) &
+#                             export.df$Variant %in% sel.var &
+#                             export.df$Virus %in% vir &
+#                             export.df$dpi == sel.dpi,]
+#sel.export.df$Virus <- factor(sapply(sel.export.df$Virus,
+#                                    function(v) {
+#                                      vir.lvls[which(vir == v)]
+#                                    }),
+#                             levels = vir.lvls)
+#
+#pos.export.df <- sel.export.df[sel.export.df$Population == pos.value,]
+#min.max.fsca <- c(min(sel.export.df$FSC.A), max(sel.export.df$FSC.A))
+#pos.export.df$FSC.A <- sample(min.max.fsca, size = nrow(pos.export.df), replace = TRUE)
 
-pos.export.df <- sel.export.df[sel.export.df$Population == pos.value,]
-min.max.fsca <- c(min(sel.export.df$FSC.A), max(sel.export.df$FSC.A))
-pos.export.df$FSC.A <- sample(min.max.fsca, size = nrow(pos.export.df), replace = TRUE)
+##Calculate global number of cells to be plotted
+##equals to min number of cells per sample | to limit set in max.cells object
+#min.cells <- floor(min(sapply(unique(sel.export.df$GID),
+#                              function(x) {
+#                                nrow(sel.export.df[sel.export.df$GID == x,])
+#                              }))/100) * 100
+#if(min.cells > max.cells) {min.cells <- max.cells}
 
-#Calculate global number of cells to be plotted
-#equals to min number of cells per sample | to limit set in max.cells object
-min.cells <- floor(min(sapply(unique(sel.export.df$GID),
-                              function(x) {
-                                nrow(sel.export.df[sel.export.df$GID == x,])
-                              }))/100) * 100
-if(min.cells > max.cells) {min.cells <- max.cells}
+##Set data.to.plot
+#data.to.plot <- do.call(rbind,
+#                        lapply(unique(sel.export.df$GID),
+#                               function(x) {
+#                                 df <- sel.export.df[sel.export.df$GID == x,]
+#                                 df[sample.int(n = nrow(df), size = min.cells),]
+#                               }))
+#
+##This 'data.to.plot' data.frame was saved as:
+#write.table(data.to.plot, paste0(export.dir, Cell_Export_2.txt),
+#            quote = FALSE, sep = "\t", col.names = TRUE)
 
-#Set data.to.plot
-data.to.plot <- do.call(rbind,
-                        lapply(unique(sel.export.df$GID),
-                               function(x) {
-                                 df <- sel.export.df[sel.export.df$GID == x,]
-                                 df[sample.int(n = nrow(df), size = min.cells),]
-                               }))
+sel.export.df <- export.df
+data.to.plot <- export.df
+
                                
 #Calculate "summary data" - % of GFP positive cells
 export.sum.df <- do.call(rbind,
@@ -131,13 +142,10 @@ p.bulk.sp <- ggplot(data = data.to.plot, aes(x = FSC.A, y = GFP, colour = Popula
                colour = alpha("black", .8), outlier.shape = NA, fill = NA, na.rm = TRUE, inherit.aes = TRUE) +
   geom_text(data = export.sum.df, aes(label = 100 * Ratio), inherit.aes = TRUE,
             x = 5, y = 2200, colour = "black", size = 1.5, fontface = "bold") +
-  coord_cartesian(ylim = c(1, 8*10^4),
-                  #xlim = c(4.7, 5.3)
-                  ) +
+  coord_cartesian(ylim = c(1, 8*10^4)) +
   guides(color = "none") +
   theme_classic() +
   theme(axis.text.y = element_text(color = "black" ,face = "plain", size = ax.tex.size),
-        #axis.text.x = element_text(color = "black" ,face = "plain", size = ax.tex.size, angle = 45),
         axis.text.x = element_blank(),
         axis.title = element_text(color = "black" , face = "bold", size = ax.tit.size),
         axis.ticks.x = element_blank(),
@@ -154,4 +162,3 @@ p.bulk.sp <- ggplot(data = data.to.plot, aes(x = FSC.A, y = GFP, colour = Popula
 ggsave(paste0(fig.path, "Figure3_B_FACS_3dpi.png"), p.bulk.sp,
        width = 150, height = 40, units = "mm", dpi = "retina",
        device = "png", limitsize = FALSE)
-
